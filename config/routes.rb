282e03007a1269e2b8604ab6a32d9cfa10089565
Kevin Leahey <kleahey@me.com>
2017-03-29 06:59:25 -0400

    Integrated user account management including ability to create, edit, and delete a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users, :controllers => { registrations: 'registrations' }
   #Create the resources route for the posts controller
   resources :posts
 
