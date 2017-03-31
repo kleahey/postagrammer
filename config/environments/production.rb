32637241020dd453a83ee3dfc29387d778b1c23d
Kevin Leahey <kleahey@me.com>
2017-03-31 13:04:02 -0400

    S3 fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
       s3_region: ENV.fetch('AWS_REGION'),
     },
     url: :s3_domain_url,
-    path: /:class/:attachment/:id_partition/:style/:filename
+    path: /:class/:attachment/:id_partition/:style/:filename,
   }
 
   # Code is not reloaded between requests.
